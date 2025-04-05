2aa2db4cc5edfb5cacc7d0f7be7b537f03b93e90
ganesha18 <63236505+ganesha18@users.noreply.github.com>
2025-04-05 14:44:54 +0500

    Add files via upload

---
+++ scheduler.py
@@ -0,0 +1,14 @@
+import schedule
+import time
+from etl_pipeline import etl_pipeline
+
+def job():
+    print("Running ETL pipeline...")
+    etl_pipeline()
+
+# Schedule the ETL pipeline to run every day at midnight
+schedule.every().day.at("00:00").do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
